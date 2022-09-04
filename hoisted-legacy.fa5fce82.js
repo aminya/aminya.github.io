5fc7ce95e233826a21ba8d516fd7f152de3da6f1
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-09-04 06:59:23 -0700

    Updates

--- hoisted-legacy.fa5fce82.js
+++
@@ -1,1 +0,0 @@
-System.register(["./chunks/collapse-legacy.407529df.js"],(function(){"use strict";return{setters:[function(){}],execute:function(){}}}));
--- hoisted.d812929c.js
+++ hoisted.d812929c.js
@@ -1,1 +1,1 @@
-import"./chunks/collapse.e24830a7.js";function t(){import("data:text/javascript,")}export{t as __vite_legacy_guard};
+import"./chunks/collapse.e24830a7.js";
